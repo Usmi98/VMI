d949dcf9e79c496f28aa4411f9bb6eeb8612c41c
M Usman Shah <70074546+Usmi98@users.noreply.github.com>
2022-11-08 11:22:58 +0500

    Add files via upload

---
+++ build/precache-manifest.dc86180b1873ae86f8a0a5b098eb6c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f39a9e98c72e94f722d1",
+    "url": "/static/css/main.09f720c5.chunk.css"
+  },
+  {
+    "revision": "f39a9e98c72e94f722d1",
+    "url": "/static/js/main.f39a9e98.chunk.js"
+  },
+  {
+    "revision": "292403534323003c79c2",
+    "url": "/static/js/1.29240353.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e25638b8130610d62ab76fa7525eff93",
+    "url": "/index.html"
+  }
+];
